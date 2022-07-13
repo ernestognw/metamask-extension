bc669f2a8de26d07fa25f3a1edf014875e5386bf
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-07-14 00:53:58 +0400

    Backup controller don't have a store.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/lib/ComposableObservableStore.js
+++ app/scripts/lib/ComposableObservableStore.js
@@ -51,6 +51,7 @@ export default class ComposableObservableStore extends ObservableStore {
   updateStructure(config) {
     this.config = config;
     this.removeAllListeners();
+    console.log("Config: ", config);
     for (const key of Object.keys(config)) {
       if (!config[key]) {
         throw new Error(`Undefined '${key}'`);
